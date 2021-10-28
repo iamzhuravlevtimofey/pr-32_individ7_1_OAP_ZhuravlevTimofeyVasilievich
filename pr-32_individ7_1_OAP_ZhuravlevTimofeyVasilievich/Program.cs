476bb50dfe0d1f64c67a9bb87ad0140998d6b27b
iamzh <iamzh@DESKTOP-7JLVQQU>
2021-10-28 15:46:08 +0500

    Добавьте файлы проекта.

---
+++ pr-32_individ7_1_OAP_ZhuravlevTimofeyVasilievich/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace pr_32_individ7_1_OAP_ZhuravlevTimofeyVasilievich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = File.ReadAllText("t.txt");
+            if (a == "" || a == " ")
+            {
+                Console.WriteLine("Файл пустой");
+            }
+            else
+            {
+                Stack<char> newstack = new Stack<char>();
+                foreach (var b in a)
+                {
+                    if (b == '#')
+                    {
+                        if (newstack.Count > 0)
+                        {
+                            newstack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        newstack.Push(b);
+                    }
+                }
+                var arr = newstack.ToArray();
+                Array.Reverse(arr);
+                string c = new string(arr);
+                Console.WriteLine(c);
+            }
+            Console.ReadLine();
+        }
+    }
+}
